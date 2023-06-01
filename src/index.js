d7f7c6777e57c57524740b8f7eb611e1bc729e63
gitirom <wadiaromdane10@gmail.com>
2023-06-01 19:32:43 +0200

    I add subscribe and completedTask functions.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import Store from "./store";
-import { addTask, removeTask } from "./action";
+import { addTask, removeTask, completedTask } from "./action";
+
+/*this function run everyTime when something changed in 
+our Redux Store state. */
+
+const unsubscribe = Store.subscribe(() => {
+    console.log("Updated", Store.getState());
+});
 
 //Dispatching action from Store
 Store.dispatch(addTask("Task 1"));
+Store.dispatch(addTask("Task 2"));
 console.log(Store.getState());
+
+unsubscribe();
+Store.dispatch(completedTask(2))
+
 Store.dispatch(removeTask(1));
+console.log(Store.getState());
+
+
 console.log(Store.getState());
--- src/reducer.js
+++ src/reducer.js
@@ -10,22 +10,28 @@
 
 */
 
-
 //creating a reducer function 
+import * as actionTypes from "./actionTypes";
 
 let id = 0 ;
 
 export default function reducer(state=[], action) {
-    if(action.type === "ADD_TASK") {
+    if(action.type === actionTypes.ADD_TASK) {
         return [...state, 
             {
                 id: ++id,
                 task: action.payload.task,
                 completed: false
             }
-        ]
+        ];
+        
     }
-    else if(action.type === "REMOVE_TASK"){
+    else if(action.type === actionTypes.TASK_COMPLETED){
+        return state.map(task => task.id === action.payload.id ? {
+            ...task, completed: true
+        } : task)
+    }
+    else if(action.type === actionTypes.REMOVE_TASK){
         return state.filter(task => task.id !== action.payload.id);
     }
 
