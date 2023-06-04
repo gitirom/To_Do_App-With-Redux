6f5dc76fa64a167215000086a311eb537480abf1
gitirom <wadiaromdane10@gmail.com>
2023-06-04 22:43:20 +0200

    Making API call using createAsyncThunk.

--- src/Store/tasks.js
+++ src/Store/tasks.js
@@ -1,30 +1,63 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 let id = 0 ;
+
+const initialState = {
+    tasks: [],
+    loading: false,
+    error: null
+}
+
+
+// Fetching data using createAsyncThunk() function they have pending property take loading, fullfiled take work part, rejected take the error using withrejectedValue()  
+
+export  const fetchTask =  createAsyncThunk("fetchTasks", async(a,{rejectWithValue}) => {
+    try {
+        const response = await axios.get("http://localhost:5000/api/tasks");
+        return { tasks: response.data }
+    } catch (error) {
+        return rejectWithValue({error: error.message})
+    }
+    
+});
 
 // create an action and reducer, which single method, which is Create Slice. 
 const taskSlice = createSlice({
     name: "tasks",
-    initialState: [],
+    initialState,
     reducers: {
         //action: function
         getTasks: (state, action) => {
-            return action.payload.tasks
+            state.tasks = action.payload.tasks
         },
         addTask: (state, action) => {
-            state.push({
+            state.tasks.push({
                 id: ++id,
                 task: action.payload.task,
                 completed: false
         })
         },
         removeTask: (state, action) => {
-            const index = state.findIndex(task => task.id === action.payload.id)
+            const index = state.tasks.findIndex(task => task.id === action.payload.id)
             state.splice(index, 1) //remove the id-task from the index 
         },
         completedTask: (state, action) => {
-            const index = state.findIndex(task => task.id === action.payload.id)
-            state[index].completed = true;
+            const index = state.tasks.findIndex(task => task.id === action.payload.id)
+            state.tasks[index].completed = true;
+        }
+    },
+    extraReducers: {  //this reducer but can using createAsyncThink 
+        [fetchTask.pending]: (state, action) => {
+            state.loading = true
+        },
+        [fetchTask.fulfilled]: (state, action) => {
+            state.tasks = action.payload.tasks
+            state.loading = false
+        },
+        [fetchTask.rejected]: (state, action) => {
+            state.error = action.payload.error
+            state.loading = false
         }
     }
 })
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 
 import Store from "./Store/ConfigureStore";
-import { getTasks } from "./Store/tasks";
+import { fetchTask, getTasks } from "./Store/tasks";
 import axios from "axios";
 
-const gettingTasks = async() => {
-    try {
-        // calling api
-        const response = await axios.get("http://localhost:5000/api/tasks");
-        console.log(response);
-        // dispatch action
-        Store.dispatch(getTasks({ tasks: response.data }))
-    } catch (error) {
-        Store.dispatch({ type: "SHOW_ERROR", payload: {error: error.message} })
-    }
-}
+// const gettingTasks = async() => {
+//     try {
+//         // calling api
+//         const response = await axios.get("http://localhost:5000/api/tasks");
+//         console.log(response);
+//         // dispatch action
+//         Store.dispatch(getTasks({ tasks: response.data }))
+//     } catch (error) {
+//         Store.dispatch({ type: "SHOW_ERROR", payload: {error: error.message} })
+//     }
+// }
 
-gettingTasks();
+// gettingTasks();
 
+Store.dispatch(fetchTask( ))
 
